963d19c7b8aafd3cb746e9318e611ac266b0ec0a
frugalcodes <samueligwe63@gmail.com>
2024-01-20 23:33:06 +0100

    deploy

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  basePath : isProd ? '/Trestour-App-LandingPage' : '',
+ 
   output : 'export',
   distDir : 'dist',
-  images : {
-    unoptimized : true,
-  },
+  
 };
 
 module.exports = nextConfig;
