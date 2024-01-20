3013d6003ae87f62f2bd4c4743220dc68a96a796
frugalcodes <samueligwe63@gmail.com>
2024-01-20 22:14:39 +0100

    deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Add the new output configuration
-  output: {
-  
-    target: 'static',
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-    target: 'experimental-serverless-trace',
+  output : 'export',
+  distDir : 'dist',
+  images : {
+    unoptimized : true,
   },
 };
 
