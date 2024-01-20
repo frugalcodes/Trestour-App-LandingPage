9ca0b0475b8469075721c2c829133c5803d46b68
frugalcodes <samueligwe63@gmail.com>
2024-01-20 12:51:56 +0100

    build

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+    basePath: '/Trestour-App-LandingPage',
+    assetPrefix: '/Trestour-App-LandingPage/',
     output: 'export',
+    // Other configurations...
   }
-   
-  module.exports = nextConfig
+  
+  module.exports = nextConfig;
+  
